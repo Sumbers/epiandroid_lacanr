60a3aecc2a18715ccbbaf0ad0e8c66f475c077c8
ruano_a <aurelien.ruano@epitech.eu>
2015-01-21 19:13:57 +0100

    first commit

---
+++ EpiAndroid/app/src/main/java/com/example/lacan/epiandroid/ConnexionActivity.java
@@ -0,0 +1,127 @@
+package com.example.lacan.epiandroid;
+
+import android.app.Activity;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ConnexionActivity extends Activity{
+    private Button connect = null;
+    private EditText loginInput = null;
+    private EditText pwdInput = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+
+        connect = (Button) findViewById(R.id.connectButton);
+        loginInput = (EditText) findViewById(R.id.editLogin);
+        pwdInput = (EditText) findViewById(R.id.editPwd);
+
+    }
+    public void enableStrictMode() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void sendInfos(View view) throws IOException {
+        String login = loginInput.getText().toString();
+        String pwd = pwdInput.getText().toString();
+
+        enableStrictMode();
+        if (login.length() > 0 && pwd.length() > 0)
+        {
+            HttpClient httpclient = new DefaultHttpClient();
+            try {
+                URI serv = new URI("https://epitech-api.herokuapp.com/");
+                HttpPost httppost = new HttpPost(serv);
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("login", login));
+                nameValuePairs.add(new BasicNameValuePair("password", pwd));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse httpresponse = httpclient.execute(httppost);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(httpresponse.getEntity().getContent()));
+                String r = reader.readLine();
+                System.out.println("String is " + r);
+            }
+            catch (URISyntaxException e) {
+                System.out.println("syntax problem");
+            }
+            catch (ClientProtocolException e){
+                System.out.println("protocole exception " + e.getMessage());
+            }
+            catch (IOException e){
+                System.out.println("io exception " + e.getMessage());
+            }
+        }
+        else {
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.toast_layout,
+                    (ViewGroup) findViewById(R.id.toast_layout_root));
+
+            TextView text = (TextView) layout.findViewById(R.id.text);
+            text.setText("Tous les champs sont requis");
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setView(layout);
+            toast.show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connexion, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
