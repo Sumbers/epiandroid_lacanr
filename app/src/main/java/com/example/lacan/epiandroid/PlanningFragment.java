9e5fe1af7627b0ec06fc66445e82ffbdf4be1fd8
Sumbers <remi.lacan@epitech.eu>
2015-02-03 21:03:56 +0100

    Planning Affichage presque fini

--- app/src/main/java/com/example/lacan/epiandroid/PlanningFragment.java
+++ app/src/main/java/com/example/lacan/epiandroid/PlanningFragment.java
@@ -6,17 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 
 public class PlanningFragment extends Fragment implements MyActivity {
@@ -25,9 +33,16 @@ public class PlanningFragment extends Fragment implements MyActivity {
     private String _session = null;
     private Date start = null;
     private Date end = null;
-    private SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
     private TextView waitView = null;
     private String infos = null;
+    private Map date_equivalent = new Hashtable();
+    private ListView activityListView = null;
+    private SimpleDateFormat formatSent = new SimpleDateFormat("yyyy-MM-dd");
+    private SimpleDateFormat formatGet = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private SimpleDateFormat formatDay = new SimpleDateFormat("EEEE");
+    private SimpleDateFormat formatHour = new SimpleDateFormat("HH");
+    private ArrayList<Map<String, String>> list = null;
+    private SimpleAdapter adapteur = null;
 
     public static PlanningFragment newInstance(String session) {
         PlanningFragment pf = new PlanningFragment();
@@ -40,15 +55,30 @@ public class PlanningFragment extends Fragment implements MyActivity {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.planning_layout, container, false);
         period = (TextView) rootview.findViewById(R.id.periode);
+        waitView = (TextView) rootview.findViewById(R.id.wait);
+        activityListView = (ListView) rootview.findViewById(R.id.listActivity);
         setTime();
+        setequiv();
         //récuperer les infos de /planning GET
         getWeekActivity();
         return rootview;
     }
 
+    void setequiv()
+    {
+        this.date_equivalent.put("Monday", "Lundi");
+
+        this.date_equivalent.put("Tuesday", "Mardi");
+        this.date_equivalent.put("Wednesday", "Mercredi");
+        this.date_equivalent.put("Thursday", "Jeudi");
+        this.date_equivalent.put("Friday", "Vendredi");
+        this.date_equivalent.put("Saturday", "Samedi");
+        this.date_equivalent.put("Sunday", "Dimanche");
+    }
+
     private void getWeekActivity() {
-        String startToSend = formater.format(this.start);
-        String endToSend = formater.format(this.end);
+        String startToSend = formatSent.format(this.start);
+        String endToSend = formatSent.format(this.end);
         System.out.println("date de départ" + startToSend);
         System.out.println("date de de fin" + endToSend);
         new ConnexionTask(this, ConnexionTask.GET, ConnexionTask.ARRAY).execute("3", "planning", "token", this._session,
@@ -70,44 +100,120 @@ public class PlanningFragment extends Fragment implements MyActivity {
         }
         else
         {
-
             try
             {
                 JSonContainer cont = new JSonContainer();
                 JSONArray ar = cont.get_array(infos);
                 JSONObject obj = null;
-                String titre = null;
-                int i =  ar.length() - 1;
-                while (i >= 0)
+                String day = "";
+                int i = 0;
+
+                //test
+                String jour = null;
+                String jourPrec = " ";
+                String room = null;
+                String heureStart = null;
+                String activity = null;
+                Map map = null;
+                int newDay = 0;
+                while (i < ar.length())
                 {
                     obj = ar.getJSONObject(i);
                     //si on est inscrit à l'activité
-                    if (obj.getString("event_registered") != "null")
+                    if (obj.getString("event_registered") != "null" && ((activity = obj.getString("acti_title")) != "null"))
                     {
-                       System.out.println("Nom activité : " + obj.getString("acti_title"));
+                       System.out.println("TOUT VA BIEN");
+                        if ((jour = getActivityDayInFrench(obj.getString("start"))) != null)
+                        {
+                            map = new Hashtable();
+                            System.out.println("TOUT VA BIEN2");
+                            if (jour.compareTo(jourPrec) != 0)
+                            {
+                               jourPrec = jour;
+                               System.out.println("TOUT VA BIEN3");
+                               map.put("jour", jour);
+
+                            }
+                            System.out.println("TOUT VA BIEN4");
+                            System.out.println("JOUR en cours : " + jour);
+                            System.out.println("Nom activité : " + activity);
+                            map.put("activité", activity);
+                            room = obj.getJSONObject("room").getString("code");
+                            map.put("room", room);
+                            System.out.println("La salle est : " + room);
+                             if ((heureStart = getStartHour(obj.getString("start"))) != null)
+                             {
+                                map.put("start", heureStart);
+                                System.out.println("Heure de départ : " + heureStart);
+                                list.add(map);
+                                System.out.println("TOUT VA BIEN5");
+                             }
+                        }
                     }
-                    i--;
+                    i++;
                 }
+                String[] from = {"jour", "activité", "room", "start"};
+                int[] to = {R.id.jour, R.id.activity, R.id.room, R.id.hour};
+                this.adapteur = new SimpleAdapter(getActivity(), list,
+                        android.R.layout.simple_list_item_1, from, to);
+                activityListView.setAdapter(this.adapteur);
+            }
+            catch (JSONException e)
+            {
+
             }
             catch (Exception e)
             {
-                e.getMessage();
+                System.err.println("ERREUR MOTHER FOCKER");
+                e.printStackTrace();
             }
         setDataToView();
         }
+    }
+
+    public String getStartHour(String way)
+    {
+        String heure = null;
+        Date dateActivity = null;
+        try
+        {
+            dateActivity = formatGet.parse(way);
+            heure = formatHour.format(dateActivity);
+        }
+        catch (Exception e)
+        {
+            return (null);
+        }
+       return (heure);
+    }
+    
+    public String getActivityDayInFrench(String way)
+    {
+        String jour = null;
+        Date dateActivity = null;
+
+        try
+        {
+            dateActivity = formatGet.parse(way);
+            jour = (String) this.date_equivalent.get(formatDay.format(dateActivity));
+        }
+        catch (Exception e)
+        {
+            return (null);
+        }
+        return (jour);
     }
 
     //set la date du début et de la fin de la semaine en cours
     public void setTime() {
         this.start = new Date();
         //récupère le jour de la semaine au format long
-        SimpleDateFormat formate = new SimpleDateFormat("EEEE");
         Calendar cale = Calendar.getInstance();
         //initialise le calendier a la date courrante
         cale.setTime(this.start);
 
         //tant que le jour n'est pas Lundi
-        while (formate.format(this.start).compareTo("Monday") != 0)
+        while (formatDay.format(this.start).compareTo("Monday") != 0)
         {
             //on retire des jours au calendrier
             cale.add(Calendar.DATE, -1);
@@ -117,7 +223,7 @@ public class PlanningFragment extends Fragment implements MyActivity {
         }
         this.end = new Date();
         cale.setTime(this.end);
-        while (formate.format(this.end).compareTo("Sunday") != 0)
+        while (formatDay.format(this.end).compareTo("Sunday") != 0)
         {
             //on ajoute des jours au calendrier
             cale.add(Calendar.DATE, 1);
@@ -129,14 +235,11 @@ public class PlanningFragment extends Fragment implements MyActivity {
 
     public void setDataToView()
     {
-        RelativeLayout tmp;
-
-        this.formater.applyPattern("dd-MM-yyyy");
-        String startToSend = formater.format(this.start);
-        String endToSend = formater.format(this.end);
+        this.formatSent.applyPattern("dd-MM-yyyy");
+        String startToSend = formatSent.format(this.start);
+        String endToSend = formatSent.format(this.end);
         System.out.println("AFFICHAGE !!");
-        tmp = ((RelativeLayout)waitView.getParent());
-        tmp.removeView(waitView);
+        ((RelativeLayout)waitView.getParent()).removeView(waitView);
        this.period.setText("Du Lundi " + startToSend +  "\nAu Lundi " + endToSend);
     }
 }
